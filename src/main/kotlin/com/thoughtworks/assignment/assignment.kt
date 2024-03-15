af702b03fcb84c0cf56257426cc94b1a0055ce91
layton <jiajun.huang@thoughtworks.com>
2024-03-16 00:55:17 +0800

    feat: add basic implement

--- src/main/kotlin/com/thoughtworks/assignment/assignment.kt
+++ src/main/kotlin/com/thoughtworks/assignment/assignment.kt
@@ -1,5 +1,22 @@
 package com.thoughtworks.assignment
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+
+fun convertToString(numm: Int): String {
+    var num = numm
+    val stringBuilder = StringBuilder()
+    while (num > 0) {
+        num--
+        val remainder = num % 26
+        stringBuilder.append((remainder + 65).toChar())
+        num /= 26
+    }
+    return stringBuilder.reverse().toString()
+}
+
+fun getSequenceList(startNums: Int, resNums: Int): List<String> {
+    val resList = ArrayList<String>()
+    for (i in startNums..<startNums + resNums) {
+        resList.add(convertToString(i))
+    }
+    return resList
 }
