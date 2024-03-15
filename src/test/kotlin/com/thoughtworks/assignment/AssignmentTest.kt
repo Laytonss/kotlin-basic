56946ca6ce2b30c853433e4f2ad1d23b23b8f556
layton <jiajun.huang@thoughtworks.com>
2024-03-16 00:54:20 +0800

    test: add basic test for assignment

--- src/test/kotlin/com/thoughtworks/assignment/AssignmentTest.kt
+++ src/test/kotlin/com/thoughtworks/assignment/AssignmentTest.kt
@@ -1,10 +1,21 @@
 package com.thoughtworks.assignment
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class AssignmentTest {
     @Test
-    fun `tmp test`() {
-        println("test")
+    fun `should return A when both startNums and resNums are 1`() {
+        assertEquals(getSequenceList(1, 1), arrayListOf("A"))
+    }
+
+    @Test
+    fun `should return A,B when startNums 1 resNums 2`() {
+        assertEquals(getSequenceList(1, 2), arrayListOf("A","B"))
+    }
+
+    @Test
+    fun `should return Z,AA,AB when startNums 26 resNums 3`() {
+        assertEquals(arrayListOf("Z", "AA", "AB"), getSequenceList(26, 3))
     }
 }
