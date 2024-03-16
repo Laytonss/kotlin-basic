12bbbee8e17d8c2daf3dcd7a41d2b167a7cbe977
layton <jiajun.huang@thoughtworks.com>
2024-03-16 15:24:59 +0800

    test: add test for Exception situation

--- src/test/kotlin/com/thoughtworks/assignment/AssignmentTest.kt
+++ src/test/kotlin/com/thoughtworks/assignment/AssignmentTest.kt
@@ -1,6 +1,7 @@
 package com.thoughtworks.assignment
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 class AssignmentTest {
@@ -11,11 +12,31 @@ class AssignmentTest {
 
     @Test
     fun `should return A,B when startNums 1 resNums 2`() {
-        assertEquals(getSequenceList(1, 2), arrayListOf("A","B"))
+        assertEquals(getSequenceList(1, 2), arrayListOf("A", "B"))
     }
 
     @Test
     fun `should return Z,AA,AB when startNums 26 resNums 3`() {
         assertEquals(arrayListOf("Z", "AA", "AB"), getSequenceList(26, 3))
     }
+
+    @Test
+    fun `should return ZZZ when startNums 18278 resNums 1`() {
+        assertEquals(arrayListOf("ZZZ"), getSequenceList(18278, 1))
+    }
+
+    @Test
+    fun `should throw Exception when the first result sequence are ZZZ`() {
+        assertThrows<IndexOutOfBoundsException> {
+            getSequenceList(18279, 1)
+        }
+    }
+
+    @Test
+    fun `should throw Exception when the result list out of ZZZ`() {
+        assertThrows<IndexOutOfBoundsException> {
+            getSequenceList(18278, 2)
+        }
+    }
+
 }
